d981e2b1733c40ee0c9aa254cca9b78b0729a498
Hugo Sage <hugosage@MacBook-Pro-de-Gogo.local>
2026-03-18 18:09:53 +0100

    feat: add swagger doc

--- src/crypto.controller.ts
+++ src/crypto.controller.ts
@@ -4,28 +4,33 @@ import type { JsonObject } from './domain/types/json-object.type';
 import { DecryptUseCase } from './usecases/decrypt.usecase';
 import { SignUseCase } from './usecases/sign.usecase';
 import { VerifyUseCase } from './usecases/verify.usecase';
+import { ApiDecrypt, ApiEncrypt, ApiSign, ApiVerify } from './crypto.controller.swagger';
 
 @Controller()
 export class CryptoController {
   constructor(private readonly encryptUseCase: EncryptUseCase, private decryptUseCase: DecryptUseCase, private signUseCase: SignUseCase, private verifyUseCase: VerifyUseCase) {}
 
   @Post('/encrypt')
+  @ApiEncrypt()
   encrypt(@Body() body: JsonObject): JsonObject {
     return this.encryptUseCase.execute(body);
   }
 
   @Post('/decrypt')
+  @ApiDecrypt()
   decrypt(@Body() body: JsonObject): JsonObject {
     return this.decryptUseCase.execute(body);
   }
 
   @Post('/sign')
+  @ApiSign()
   sign(@Body() body: JsonObject): JsonObject {
    return this.signUseCase.execute(body)
   }
 
   @Post('/verify')
   @HttpCode(204)
+  @ApiVerify()
   verify(@Body() body: JsonObject): void {
     const isValid = this.verifyUseCase.execute(body);
     if (!isValid) throw new BadRequestException('Invalid signature');
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { CryptoModule } from './crypto.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(CryptoModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Crypto API')
+    .setDescription('HTTP API for encryption, decryption, signing, and verification of JSON payloads.')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
